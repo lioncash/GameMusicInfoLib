6a9bb44b9281212c50e54d4bc5755a1efe5ce271
Lioncash <mathew1800@gmail.com>
2014-01-13 18:42:06 -0500

    [IT Modules] Remove a TODO. Sample reading is already implemented.

--- GameMusicInfoReader/Modules/ITReader.cs
+++ GameMusicInfoReader/Modules/ITReader.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 
 namespace GameMusicInfoReader.Modules
 {
-	// TODO: Possibly more sample reading.
-
 	/// <summary>
 	/// A reader for Impulse Tracker modules
 	/// </summary>
